7e0f4b0d2aa60c0b87ba1e415ce3bfca9780211d
pr-124 <112579333+pr-124@users.noreply.github.com>
2022-12-17 02:05:39 +0000

    Add files

--- Creating_table.py
+++ Creating_table.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
 import sqlite3
-# establishing connection 
-conn = sqlite3.connect('test.db')
-#creating table 
-conn.execute('''CREATE TABLE COMPANY
-         (ID INT PRIMARY KEY     NOT NULL,
-         NAME           TEXT    NOT NULL,
-         AGE            INT     NOT NULL,
-         ADDRESS        CHAR(50),
-         SALARY         REAL);''')
 
-#closing connection 
+# establishing connection
+conn = sqlite3.connect("test.db")
+# creating table
+conn.execute(
+    """CREATE TABLE COMPANY
+         (ID INT PRIMARY KEY,
+         NAME TEXT    NOT NULL,
+         AGE INT     NOT NULL,
+         ADDRESS  CHAR(50),
+         SALARYn REAL);"""
+)
+
+# closing connection
 conn.close()
-
--- Select.py
+++ Select.py
@@ -2,15 +2,14 @@
 
 import sqlite3
 
-conn = sqlite3.connect('test.db')
-print "Opened database successfully";
+conn = sqlite3.connect("test.db")
 
 cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
 for row in cursor:
-   print "ID = ", row[0]
-   print "NAME = ", row[1]
-   print "ADDRESS = ", row[2]
-   print "SALARY = ", row[3], "\n"
+    print("ID = " + str(row[0]))
+    print("NAME = " + str(row[1]))
+    print("ADDRESS = " + str(row[2]))
+    print("SALARY = " + str(row[3]))
 
-print "Operation done successfully";
+
 conn.close()
